81f93c046f2eb21c4aa1f39613cd38247ae4306a
nageshkulkarni17 <nkulkarni@qwinix.io>
2019-02-01 23:34:01 +0530

    wishlist name change

--- src/reducers/favourite.js
+++ src/reducers/favourite.js
@@ -6,7 +6,7 @@ import { bake_cookie, read_cookie } from 'sfcookies';
 
 export default (state = [], action) => {
   let articles = null;
-  state = read_cookie('news');
+  state = read_cookie('article');
   switch (action.type) {
     case FAVOURITE_ARTICLE_ADDED:
     articles = [...state, action.article];
